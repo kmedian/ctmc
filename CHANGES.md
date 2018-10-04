cdd59002386ab2ab76b918e2ab7a141edfdc72a8
UH <554c46@gmail.com>
2018-10-04 21:18:44 +0200

    Comply to sklearn's instantiation rule, http://scikit-learn.org/stable/developers/contributing.html#instantiation

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,6 @@
+# 0.1.2 / 2018-10-04
+
+* Comply to sklearn's instantiation rule, http://scikit-learn.org/stable/developers/contributing.html#instantiation
 
 # 0.1.1 / 2018-09-25
 
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='ctmc',
-      version='0.1.1',
+      version='0.1.2',
       description='Continous Time Markov Chain',
       long_description=read('README.md'),
       long_description_content_type='text/markdown',
